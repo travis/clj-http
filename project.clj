c2fafd55ebdea5671d81153f8bfc16c618d5ae80
Matt Revelle <mrevelle@gmail.com>
2011-01-30 22:05:07 -0800

    Woven repos.

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
 (defproject clj-http "0.1.2-SNAPSHOT-CHUNK"
-  :description
-    "A Clojure HTTP library wrapping the Apache HttpComponents client."
-  :dependencies
-    [[org.clojure/clojure "1.2.0"]
-     [org.clojure/clojure-contrib "1.2.0"]
-     [org.apache.httpcomponents/httpclient "4.0.3"]
-     [commons-codec "1.4"]
-     [commons-io "1.4"]]
-  :dev-dependencies
-    [[swank-clojure "1.2.0"]
-     [ring/ring-jetty-adapter "0.3.5"]
-     [ring/ring-devel "0.3.5"]])
+  :description "A Clojure HTTP library wrapping the Apache HttpComponents client."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.apache.httpcomponents/httpclient "4.0.3"]
+                 [commons-codec "1.4"]
+                 [commons-io "1.4"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]
+                     [ring/ring-jetty-adapter "0.3.5"]
+                     [ring/ring-devel "0.3.5"]]
+  :repositories {"snapshots" "http://mvn.getwoven.com/repos/woven-public-snapshots"
+                 "releases" "http://mvn.getwoven.com/repos/woven-public-releases"})
